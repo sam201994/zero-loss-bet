eac0db1cf81452773d8ea62c65e94c61cd8e909a
Smriti Goel <smritigoel93@gmail.com>
2023-05-13 19:21:33 +0530

    fixed hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,24 +1,22 @@
-require("@nomicfoundation/hardhat-toolbox");
-require( "dotenv/config");
-
-const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY
+require("@nomicfoundation/hardhat-toolbox")
+require("dotenv/config")
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const { GOERLI_PRIVATE_KEY, ACCOUNT_PRIVATE_KEY } = process.env
 
 module.exports = {
 	networks: {
 		hardhat: {
 			forking: {
-				url: `https://eth-goerli.g.alchemy.com/${GOERLI_PRIVATE_KEY}`
-			}
+				url: `https://eth-goerli.g.alchemy.com/v2/${GOERLI_PRIVATE_KEY}`,
+	    },
+			chainId: 31337,
 		},
-    goerli: {
-      url: `https://eth-goerli.g.alchemy.com/${GOERLI_PRIVATE_KEY}`,
-      accounts: {
-        mnemonic: "YOUR_METAMASK_MNEMONIC",
-      },
-    },
-  },
-  solidity: "0.8.18",
-};
-
+		goerli: {
+			url: `https://eth-goerli.g.alchemy.com/v2/${GOERLI_PRIVATE_KEY}`,
+			accounts: [ACCOUNT_PRIVATE_KEY],
+	  },
+	},
+	solidity: "0.8.18",
+}
